61ce86278a8270777f69910246beaaa3eec36c34
Rqzvqn <escumicrazvan@yahoo.com>
2021-10-01 17:04:32 +0200

    login preview

--- src/main/java/nl/inholland/javafx/App.java
+++ src/main/java/nl/inholland/javafx/App.java
@@ -1,23 +1,55 @@
 package nl.inholland.javafx;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
+
+
     @Override
     public void start(Stage window) throws Exception {
-        window.setHeight(600);
-        window.setWidth(800);
-        window.setTitle("Inholland JavaFX Starter Project");
+        window.setHeight(200);
+        window.setWidth(250);
+        window.setTitle("Login");
 
-        BorderPane pane = new BorderPane();
-        Label hello = new Label("Hello World");
-        pane.setCenter(hello);
+        GridPane gridPane = new GridPane();
 
-        Scene scene = new Scene(pane);
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        gridPane.setVgap(10); // Vertical spacing between grid items
+        gridPane.setHgap(8); // Horizontal spacing between grid items
+
+        Label userLabel = new Label ("Username:");
+        GridPane.setConstraints(userLabel, 0, 0); // first column, first row
+
+        TextField userInput = new TextField();
+        System.out.println(userInput.getText());
+        userInput.setPromptText("username");
+
+        Label passwordLabel = new Label("Password:");
+        PasswordField pwField = new PasswordField();
+        pwField.setPromptText("Enter password");
+
+        String password = pwField.getText();
+
+        Button loginButton = new Button("Log in");
+
+        GridPane.setConstraints(userLabel, 0, 0);
+        GridPane.setConstraints(userInput, 1, 0);
+        GridPane.setConstraints(passwordLabel, 0, 1);
+        GridPane.setConstraints(pwField, 1, 1);
+        GridPane.setConstraints(loginButton, 1, 2); // Right align in the grid
+
+        gridPane.getChildren().addAll(userLabel, userInput, passwordLabel, pwField, loginButton);
+
+        Scene scene = new Scene(gridPane);
         window.setScene(scene);
         window.show();
     }
